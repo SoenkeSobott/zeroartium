337c1883d86f5671d7bff8f22d54386232c7efa7
Soenke Sobott <sobott.soenke@gmail.com>
2021-11-23 18:19:55 +0100

    ZERO: Added hooper credits

--- src/components/layout/ThirdPartyContent.vue
+++ src/components/layout/ThirdPartyContent.vue
@@ -2,40 +2,53 @@
   <div>
     <div>
       {{ $t("message.iconsCreatedWith") }}
-      <a href="https://www.freepik.com" title="Freepik" target="_blank">Freepik</a>
+      <a href="https://www.freepik.com" title="Freepik" target="_blank"
+        >Freepik</a
+      >
       von
-      <a href="https://www.flaticon.com/de/" title="Flaticon" target="_blank">www.flaticon.com</a>
+      <a href="https://www.flaticon.com/de/" title="Flaticon" target="_blank"
+        >www.flaticon.com</a
+      >
     </div>
     <br />
     <div>
       {{ $t("message.images") }}:
       <br />
       <ul>
-        <li> <!-- Epic figure woman -->
+        <li>
+          <!-- Epic figure woman -->
           <a href="https://unsplash.com/@sur_le_misanthrope" target="_blank">
             Pavel Nekoranec
-            <a href="https://unsplash.com/photos/I__QKQLMIKs" target="_blank">(Unsplash)</a></a
+            <a href="https://unsplash.com/photos/I__QKQLMIKs" target="_blank"
+              >(Unsplash)</a
+            ></a
           >
         </li>
         <li>
           <!-- Romanian temple -->
           <a href="https://unsplash.com/@czermak_photography" target="_blank">
             Christopher Czermak
-            <a href="https://unsplash.com/photos/PamFFHL6fVY" target="_blank">(Unsplash)</a></a
+            <a href="https://unsplash.com/photos/PamFFHL6fVY" target="_blank"
+              >(Unsplash)</a
+            ></a
           >
         </li>
         <li>
           <!-- Dirty Hands -->
           <a href="https://unsplash.com/@eseamau" target="_blank">
             Amauri Mejía
-            <a href="https://unsplash.com/photos/IhXrWDckZOQ" target="_blank">(Unsplash)</a></a
+            <a href="https://unsplash.com/photos/IhXrWDckZOQ" target="_blank"
+              >(Unsplash)</a
+            ></a
           >
         </li>
         <li>
           <!-- Art Gallery Image-->
           <a href="https://unsplash.com/@adrienolichon" target="_blank">
             Adrien Olichon
-            <a href="https://unsplash.com/photos/AcLG4W3dXUA" target="_blank">(Unsplash)</a></a
+            <a href="https://unsplash.com/photos/AcLG4W3dXUA" target="_blank"
+              >(Unsplash)</a
+            ></a
           >
         </li>
       </ul>
@@ -46,8 +59,16 @@
       <ul>
         <li>
           Cookie Banner:
-          <a href="https://www.npmjs.com/package/vue-cookie-accept-decline" target="_blank">
+          <a
+            href="https://www.npmjs.com/package/vue-cookie-accept-decline"
+            target="_blank"
+          >
             vue-cookie-accept-decline</a
+          >
+        </li>
+        <li>
+          <a href="https://baianat.github.io/hooper/" target="_blank">
+            Hooper Vue</a
           >
         </li>
       </ul>
